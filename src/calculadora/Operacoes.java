f4f88daa275fca99fdea0a11090a30fde637c2fd
Bea <biarufino9@gmail.com>
2019-03-27 22:37:31 -0300

    Bea - Adicionando subtracao e arrumando alguns codigos do main

--- src/calculadora/Operacoes.java
+++ src/calculadora/Operacoes.java
@@ -1,10 +1,15 @@
 package calculadora;
 public class Operacoes {
     float soma;
+    float subtrai;
     
     public float Soma(float x, float y){
         this.soma=x+y;
         return this.soma;
 }   
+    public float Subtrai(float x, float y){
+        this.subtrai=x-y;
+        return this.subtrai;
+} 
 }
 
