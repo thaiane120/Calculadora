7044c0666d1579431806b90522d2746392138bda
Carvalho <kfcarvalho.018@gmail.com>
2019-03-28 22:58:21 -0300

    Modificando main

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,33 +1,44 @@
 package calculadora;
 
-import javax.swing.JOptionPane;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Calculadora {
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException {
     Operacoes operacoes = new Operacoes();
-    int x, y, z;
-    x = Integer.parseInt(JOptionPane.showInputDialog("DIGITE O PRIMEIRO NUMERO: "));
-    y = Integer.parseInt(JOptionPane.showInputDialog("DIGITE O SEGUNDO NUMERO: "));
-    z = Integer.parseInt(JOptionPane.showInputDialog("*****DIGITE*****\n\n1- SOMAR\n2- SUBTRAIR\n3- MULTIPLICAR\n 4- DIVIDIR\n\n"));
-        if(z==1){
-            System.out.println("O resultado é " + operacoes.Soma(x,y));
-        }else{
-            if(z==2){
-                System.out.println("O resultado é "+operacoes.Subtrai(x, y));
-            }else{
-                if (z == 3){
-                    System.out.println("O resultado é " + operacoes.Multiplica(x, y));
-                }else{
-                    if(z == 4){
-                        System.out.println("O resultado é " + operacoes.Divide(x, y));
-                    }else{
-                       System.out.println("***OPERAÇÃO INVÁLIDA***");
-                    }
-                }
-                   
-            }
-        } 
+    float x,y,z = 0;
+        String escolha;
+        Scanner input = new Scanner(System.in);
+        do{
+            System.out.println(" Calculadora Básica: ");
+            System.out.println(":");
+            x = input.nextFloat();
+            escolha = input.next();
+            y = input.nextFloat();
             
+            if("+".equals(escolha)){
+            System.out.println("O resultado é " + operacoes.Soma(x,y));
+            }else{
+                if("-".equals(escolha)){
+                    System.out.println("O resultado é "+operacoes.Subtrai(x, y));
+                }else{
+                    if("*".equals(escolha)){
+                        System.out.println("O resultado é " + operacoes.Multiplica(x, y));
+                    }else{
+                        if("/".equals(escolha)){
+                            System.out.println("O resultado é " + operacoes.Divide(x, y));
+                        }else{
+                           System.out.println("***OPERAÇÃO INVÁLIDA***");
+                        }
+                    }
+
+                }
+            } 
+         System.out.println("Precione Enter para continuar...");
+         System.in.read();
+         System.out.println("\n\n\n\n\n");   
+        }while(z!=2);
+        
     }
 }
 
